425800e807aecabecadeaab6488527214c5edadb
Brian Joseph <brianjoseph8132@gmail.com>
2025-06-13 11:10:41 +0300

    removed app.css import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
